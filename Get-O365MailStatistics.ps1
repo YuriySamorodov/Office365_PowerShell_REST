4787eb8b7dadbd19f87887a0a8cee3b61f1d2f4c
Yuriy Samorodov <yuriy.samorodov@gmail.com>
2016-03-24 06:50:06 -0700

    First Commit

---
+++ Get-O365MailStatistics.ps1
@@ -0,0 +1,70 @@
+﻿#Mail
+
+#Varibles
+
+$Login = 'viastak@bie-executive.com'
+$Password = 'C1sP4l6*1' | ConvertTo-SecureString -AsPlainText -Force
+$UserCredential = New-Object System.Management.Automation.PSCredential( $Login , $Password )
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session
+$restUri = 'https://outlook.office365.com/api/beta/users'
+
+
+#$users = Get-Mailbox
+
+$startDate = Get-Date '01/01/2015' -Format yyyy-MM-dd
+$endDate = Get-Date '01/10/2016' -Format yyyy-MM-dd
+
+$search = '`$select=SendTime,ReceivedDateTime,Sender,ToRecipients,BCCRecipients,Subject'
+
+
+#Checking time
+
+if ( $startDate = $null ) {
+
+    $filter = "`$filter=ReceivedDateTime le $endDate"
+
+}
+
+elseif ( $endDate = $null ) {
+
+    $filter = "`$filter=ReceivedDateTime ge $startDate"
+
+}
+
+$filter = "`$filter=ReceivedDateTime ge $startDate and ReceivedDateTime le $endDate"
+
+
+if ( $startDate -eq $null -and $endDate -eq $null ) {
+
+    $filter = $null
+
+}
+
+
+
+
+#Start Date should not be newer than end date
+
+if ( $endDate > $startDate ) {
+
+    Write-Error -Message 'Start date should be newer than end date' -WarningAction Stop -Category InvalidData
+
+}
+
+
+foreach ( $user in 'ben.hawkins@bie-executive.com' ) {
+    
+    $results = @()
+        
+    $mailbatch = Invoke-RestMethod -Uri "$restUri/$user/messages?$filter" -Credential $UserCredential -Method Get ; $results = $mailbatch.value
+     
+    do { $mailbatch = Invoke-RestMethod -Uri $mailbatch.'@odata.nextLink' -Credential $UserCredential -Method Get ; $results += $mailbatch.value
+      
+      }
+
+      until ( $mailbatch.'@odata.nextLink' -eq $null )
+
+}
+
+$results| select SentDateTime, ReceivedDateTime, @{ n = 'Sender' ; e = { $_.Sender.EmailAddress.Address } }, @{ n = 'ToRecipients' ; e = { $_.ToRecipients.EmailAddress | %{ $_.Address } } }, Subject
