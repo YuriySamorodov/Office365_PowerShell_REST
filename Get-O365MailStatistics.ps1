351dde4083aee65928471e535c8f5f483eaca285
Yuriy Samorodov <Yuriy.Samorodov@gmail.com>
2016-03-25 03:17:41 +0400

    Update Get-O365MailStatistics.ps1
    Fixed test login name

--- Get-O365MailStatistics.ps1
+++ Get-O365MailStatistics.ps1
@@ -2,7 +2,7 @@
 
 #Varibles
 
-$Login = 'viastak@'
+$Login = 'viastak@bie-executive.com-'
 $Password = 'Password1' | ConvertTo-SecureString -AsPlainText -Force
 $UserCredential = New-Object System.Management.Automation.PSCredential( $Login , $Password )
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
