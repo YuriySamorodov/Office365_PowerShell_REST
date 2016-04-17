2a759e4ae1ff4760a1ddbf33920b83e11dd02674
Yuriy Samorodov <Yuriy.Samorodov@gmail.com>
2016-04-18 01:07:18 +0300

    Create Get-O365CalendarStatistics.ps1

---
+++ Get-O365CalendarStatistics.ps1
@@ -0,0 +1,90 @@
+#Mail
+
+
+Param (
+
+    
+    [Parameter(Mandatory = $true,Position = 1 )]
+    [string]$FilePath,
+    
+    [Parameter(Mandatory = $true)]
+    [datetime]$StartDate,
+    
+    [Parameter(Mandatory = $true)]
+    [datetime]$EndDate
+
+
+)
+
+
+#Varibles
+$restUri = 'https://outlook.office365.com/api/beta/users'
+$UserCredential = Get-Credential
+
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session
+$users = Get-Mailbox -Filter { RecipientTypeDetails -ne 'DiscoveryMailbox'  } -SortBy DisplayName
+
+$Start = Get-Date $StartDate -Format yyyy-MM-dd
+$End = Get-Date $EndDate -Format yyyy-MM-dd
+
+$select = 'Organizer,Attendees,ResponseStatus,Start,End,Subject'
+$filter = "CreatedDateTime ge $Start and CreatedDateTime le $End"
+
+
+#Checking time
+
+
+#Checking time - Powershell Switch
+<#
+if ( $startDate = $null ) {
+
+    $filter = "`$filter=ReceivedDateTime le $endDate"
+
+}
+
+elseif ( $endDate = $null ) {
+
+    $filter = '`$filter=ReceivedDateTime ge $startDate'
+
+}
+
+
+
+if ( $startDate -eq $null -and $endDate -eq $null ) {
+
+    $filter = $null
+
+}
+
+
+
+
+#Start Date should not be newer than end date
+
+if ( $endDate > $startDate ) {
+
+    Write-Error -Message 'Start date should be newer than end date' -WarningAction Stop -Category InvalidData
+
+}
+
+
+#>
+
+
+
+$results = @()
+
+foreach ( $user in $users | ForEach-Object UserPrincipalName ) {
+    
+    $top = 25
+    $skip = 0
+     
+    do {          
+         $batch = Invoke-RestMethod -Method Get -Uri "$restUri/$user/events?`$top=$top&`$skip=$skip&`$select=$select&`$filter=$filter" -Credential $UserCredential
+         $results += $batch.value | select *, @{ Name = 'User' ; Expression ={ $user  }  }, @{ Name = 'CalendarName' ; Expression = { ( Invoke-RestMethod -Method GET -Uri $_.'Calendar@odata.navigationLink' -Credential $UserCredential ).Name }  }
+         $skip += 25
+       }  until ( $batch.'@odata.nextLink' -eq $null )
+}
+
+$results | select  user, CalendarName, @{ n = 'Organizer' ; e = { $_.Organizer.EmailAddress.Address } }, @{ n = 'Attendees' ; e = { $_.Attendees.EmailAddress | %{ $_.Address } } }, @{ n = 'Start' ; e = { $_.Start.DateTime } }, @{ n = 'End' ; e = { $_.End.DateTime } } @{ n = 'ResponseSatus' ; e = { $_.ResponseStatus.Response } }, @{ n = 'ResponseTime' ; e = { $_.ResponseStatus.Time } } | Export-Csv -NoTypeInformation test.csv
