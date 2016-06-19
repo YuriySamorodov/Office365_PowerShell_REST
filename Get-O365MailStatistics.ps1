df8de69cd38ce913b6fdca8ce87bd6edfcd2c37b
Yuriy Samorodov <yuriy.samorodov@outlook.com>
2016-06-19 22:34:36 +0300

    Added progress bar

--- Get-O365MailStatistics.ps1
+++ Get-O365MailStatistics.ps1
@@ -17,10 +17,10 @@ Param (
 
     
     [Parameter(Mandatory = $true,Position = 1 )]
-    [string]$FilePath = '',
+    [string]$FilePath = ( Join-Path "$($env:USERPROFILE)\Desktop" -ChildPath ( ( Get-Date -Format yyyy-MM-dd ) + '_' + 'V11-6-16' + '_' + 'MailStats' + '.csv' ) ),
     
     [Parameter(Mandatory = $false)]
-    [datetime]$StartDate = 0,
+    [string]$StartDate = '2001-01-01',
     
     [Parameter(Mandatory = $false)]
     [datetime]$EndDate = ( Get-Date ).AddDays(1)
@@ -28,87 +28,70 @@ Param (
 
 )
 
+BEGIN {
 
-#Varibles
+#region Variables
+
     $restUri = 'https://outlook.office365.com/api/beta/users'
     $UserCredential = Get-Credential
 
     $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
     Import-PSSession $Session -AllowClobber
-$users = Get-Mailbox -Filter { RecipientTypeDetails -ne 'DiscoveryMailbox'  } -SortBy DisplayName
+    $users = Get-Mailbox -Filter { RecipientTypeDetails -ne 'DiscoveryMailbox'  } -SortBy DisplayName
 
-$Start = Get-Date $StartDate -Format yyyy-MM-dd
-$End = Get-Date $EndDate -Format yyyy-MM-dd
+    $Start = Get-Date $StartDate -Format yyyy-MM-dd
+    $End = Get-Date $EndDate -Format yyyy-MM-dd
 
-$select = 'SentDateTime,ReceivedDateTime,Sender,ToRecipients,BCCRecipients,Subject'
-$filter = "ReceivedDateTime ge $Start and ReceivedDateTime le $End"
-
-
-#Checking time
-
-$startDate = Get-Date '01/01/2015' -Format yyyy-MM-dd
-$endDate = Get-Date '03/10/2016' -Format yyyy-MM-dd
-
-
-#Checking time - Powershell Switch
-<#
-if ( $startDate = $null ) {
-
-    $filter = "`$filter=ReceivedDateTime le $endDate"
+    $select = 'SentDateTime,ReceivedDateTime,Sender,ToRecipients,CCRecipients,BCCRecipients,Subject'
+    $filter = "ReceivedDateTimege $Start and ReceivedDateTime le $End"
+        
+#endregion
 
 }
 
-elseif ( $endDate = $null ) {
+PROCESS {
 
-    $filter = '`$filter=ReceivedDateTime ge $startDate'
+#region Processing
 
-}
-
-
-
-if ( $startDate -eq $null -and $endDate -eq $null ) {
-
-    $filter = $null
-
-}
-
-
-
-
-#Start Date should not be newer than end date
-
-if ( $endDate > $startDate ) {
-
-    Write-Error -Message 'Start date should be newer than end date' -WarningAction Stop -Category InvalidData
-
-}
-
-
-#>
-
-
-$results = @()
-
-foreach ( $user in $users | ForEach-Object UserPrincipalName ) {
+    $results = @(
     
-    $top = 25
-    $skip = 0
+    for ( $i = 0 ; $i -lt $users.Count ; $i++ ) {
 
-    #$mailbatch = Invoke-RestMethod -Uri "$restUri/$user/messages?`$filter=$filter&`$select=$select&`$top=$top" -Credential $UserCredential -Method Get ; $results = $mailbatch.value
-     
-    do {          
-         $batch = Invoke-RestMethod -Method Get -Uri "$restUri/$user/messages?`$filter=$filter&`$select=$select&`$top=$top&`$skip=$skip" -Credential $UserCredential
-         $results += $batch.value
-         $skip += 25
+
+        $ProgressProperties = [psobject]@{
+                                        
+            Activity = 'Getting mail information'
+            CurrentOperation = "$Percent% complete"
+            PercentComplete = $Percent = [math]::Round( ( $i / $users.Count * 100 ) , 2 )
+            Status = $users[$i].UserPrincipalName
+                                        
+            }
+
+        Write-Progress @ProgressProperties
+    
+        $top = 25
+        $skip = 0
+
+        do {          
+             $batch = Invoke-RestMethod -Method Get -Uri "$restUri/$($users[$i].UserPrincipalName)/messages?&`$filter=$filter&`$select=$select&`$top=$top&`$skip=$skip" -Credential $UserCredential
+             $results += $batch.value
+             $skip += 25
        
-       }
+           }
 
-      until ( $batch.'@odata.nextLink' -eq $null )
+          until ( $batch.'@odata.nextLink' -eq $null )
+
+    }
+
+#endregion
+
 
 }
 
+END {
 
-$results | select SentDateTime, ReceivedDateTime, @{ n = 'Sender' ; e = { $_.Sender.EmailAddress.Address } }, @{ n = 'ToRecipients' ; e = { $_.ToRecipients.EmailAddress | %{ $_.Address } } }, Subject | Export-csv -NoTypeInformation $FilePath
+$results | select SentDateTime, ReceivedDateTime, @{ n = 'Sender' ; e = { $_.Sender.EmailAddress.Address } }, @{ n = 'ToRecipients' ; e = { $_.ToRecipients.EmailAddress | %{ $_.Address } } }, @{ n = 'CCRecipients' ; e = { $_.CCRecipients.EmailAddress | %{ $_.Address } } },  @{ n = 'BccRecipients' ; e = { $_.BccRecipients.EmailAddress | %{ $_.Address } } }, Subject |Export-csv -NoTypeInformation $FilePath
+
 
 #region Cleanup
 
@@ -116,3 +99,6 @@ Get-PSSession | Remove-PSSession
 Remove-Variable Session
 
 #endregion
+
+
+}
